905060375c99439dc39e4503faa0d1ad0feaad04
nicotognella <nicolastognella@gmail.com>
2021-06-05 19:34:28 +1000

    modificacion5

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let express = require ('express');
-let app = express ();
+let express = require('express');
+let app = express();
 
-let path = require ('path') 
- // seteamos los archivos publicos para poder acceder a esta carpeta con imagenes y hojas de estilo
-// si no lo seteamos el servidor va a tomar public como una carpeta privada y no me lo va a mostrar
+let path = require('path')
+    // seteamos los archivos publicos para poder acceder a esta carpeta con imagenes y hojas de estilo
+    // si no lo seteamos el servidor va a tomar public como una carpeta privada y no me lo va a mostrar
 let publicPath = path.resolve(__dirname, './public')
-app.use (express.static (publicPath))
+app.use(express.static(publicPath))
 
-app.listen (4000, () => console.log ("todo ok"));
+app.listen(4001, () => console.log("todo ok"));
 
-app.get ('/', (req, res)=> {
-    res.sendFile (path.resolve (__dirname, './views/home.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
