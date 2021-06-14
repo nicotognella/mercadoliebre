5c62500187dcbe0348ce65684a0851b2369ddb6a
nicotognella <nicolastognella@gmail.com>
2021-06-15 09:35:18 +1000

    agregue formularios2

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
-app.get('/', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
